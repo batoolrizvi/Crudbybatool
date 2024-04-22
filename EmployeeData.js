3de0b744e4c489dee7e0715fe495134c9140cc0a
batoolrizvi <156038526+batoolrizvi@users.noreply.github.com>
2024-04-22 17:43:20 +0500

    Add files via upload

---
+++ EmployeeData.js
@@ -0,0 +1,30 @@
+export const Data = [
+    {
+        id:12,
+        firstName:"Ali",
+        lastName:"Ahmad",
+        age:22
+
+    },
+    {
+        id:13,
+        firstName:"Ali",
+        lastName:"Jee",
+        age:24
+
+    },
+    {
+        id:14,
+        firstName:"HassanRaza",
+        lastName:"Rizvi",
+        age:24
+
+    },
+    {
+        id:10,
+        firstName:"SyedaKainat",
+        lastName:"BatoolRizvi",
+        age:24
+    }
+
+]
